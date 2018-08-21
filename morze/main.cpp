82016d6b192dc67c647f7b87c2fcc3f2499620a8
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2018-08-21 11:23:11 +0300

    Send timestamps instead of bools

--- morze/main.cpp
+++ morze/main.cpp
@@ -15,6 +15,7 @@
 struct signal
 {
 	std::deque<bool> s;
+	int current_time = 0;
 
 	signal(int size)
 	{
@@ -22,19 +23,35 @@ struct signal
 			s.push_back(false);
 	}
 
-	void update_cache(bool is_pressed)
+	void set_current_time(int t)
 	{
-		s.push_back(is_pressed);
-		s.pop_front();
+		current_time = t;
+		for (auto it = s.begin(); it != s.end(); ++it)
+			*it = false;
+	}
+
+	void update_cache(int time_stamp)
+	{
+		int index = time_stamp - current_time;
+		if (index >= 0 && index < static_cast<int>(s.size()))
+			s[index] = true;
+	}
+
+	void iterate()
+	{
+		s.push_front(false);
+		s.pop_back();
+		++current_time;
 	}
 
 	void draw(sf::RenderTarget &window, sf::RectangleShape &line)
 	{
 		int i = 0;
-		for (auto it = s.rbegin(); it != s.rend(); ++it, ++i)
+		float const width = window.getView().getSize().x;
+		for (auto it = s.rbegin(); it != s.rend() && i < width; ++it, ++i)
 			if (*it)
 			{
-				line.setPosition({static_cast<float>(i), 0});
+				line.setPosition({width - static_cast<float>(i), 0});
 				window.draw(line);
 			}
 	}
@@ -58,9 +75,9 @@ struct socket_sender
 		port = port1;
 	}
 
-	void send(bool is_pressed)
+	void send(int time_stamp)
 	{
-		socket.send(&is_pressed, 1, addr, port);
+		socket.send(&time_stamp, sizeof(int), addr, port);
 	}
 };
 
@@ -76,14 +93,14 @@ struct socket_receiver
 		client.bind(port);
 	}
 
-	bool receive()
+	bool receive(int &time_stamp)
 	{
-		bool is_pressed = false;
 		size_t received = 0;
 		sf::IpAddress remote_addr;
 		unsigned short remote_port;
-		client.receive(&is_pressed, 1, received, remote_addr, remote_port);
-		return is_pressed;
+		auto code = client.receive(&time_stamp, sizeof(int), received, remote_addr, remote_port);
+		//std::cout << code << " " << time_stamp << std::endl;
+		return code == sf::Socket::Status::Done;
 	}
 };
 
@@ -102,9 +119,11 @@ int main(int argc, char **argv)
 	};
 	//window.setVerticalSyncEnabled(true);
 
+	int const signals_per_second = 100;
+
 	sf::Clock clock;
 	sf::Time time;
-	sf::Time stepTime = sf::seconds(1.0 / 100);
+	sf::Time stepTime = sf::seconds(1.0 / signals_per_second);
 
 	sf::Font font;
 	if (!font.loadFromFile("/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-B.ttf"))
@@ -121,7 +140,8 @@ int main(int argc, char **argv)
 	sf::RectangleShape line({1.0f, height});
 
 	signal send_signal(width);
-	signal receive_signal(width);
+	signal receive_signal(width + signals_per_second);
+	receive_signal.set_current_time(10000);
 
 	socket_sender sender(argv[2], std::stoi(argv[3]));
 	socket_receiver receiver(std::stoi(argv[1]));
@@ -147,9 +167,27 @@ int main(int argc, char **argv)
 		while (time > stepTime)
 		{
 			time -= stepTime;
-			send_signal.update_cache(is_pressed);
-			sender.send(is_pressed);
-			receive_signal.update_cache(receiver.receive());
+
+			if (is_pressed)
+			{
+				send_signal.update_cache(send_signal.current_time);
+				//std::cout << "sending " << send_signal.current_time << "\n";
+				sender.send(send_signal.current_time);
+			}
+
+			int time_stamp = 0;
+			while (receiver.receive(time_stamp))
+			{
+				//std::cout << "received " << time_stamp << "\n";
+				if (time_stamp + static_cast<int>(receive_signal.s.size()) < receive_signal.current_time)
+					receive_signal.set_current_time(time_stamp);
+				receive_signal.update_cache(time_stamp);
+				receive_signal.update_cache(time_stamp - 1);
+				receive_signal.update_cache(time_stamp + 1);
+			}
+
+			send_signal.iterate();
+			receive_signal.iterate();
 		}
 
 		fps_timestamps.push_back(fps_clock.getElapsedTime());
